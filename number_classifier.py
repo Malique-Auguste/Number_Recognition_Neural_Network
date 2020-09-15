ae3888a2139764d6274fd73d9a6535657e3dd591
Malique_Auguste <maliqueauguste@gmail.com>
2020-09-15 14:24:54 -0400

    optimised code

--- number_classifier.py
+++ number_classifier.py
@@ -3,12 +3,14 @@ import os
 import sys
 import pickle
 import display
+import time
 
-train = False
+train = True
 
 #Input and ouput values that the neural network will train on
-x_list = np.array([])
-y_list = np.array([])
+x_list = np.array([[1, 1, 1], [1, 0, 0], [1, 1, 0], [1, 0, 1]])
+y_list = np.array([[1], [1], [0], [0]])
+new_y_list = []
 
 #weights
 parameters = []
@@ -22,6 +24,7 @@ total_delta = []
 
 #x_propogated is often denoted as the 'activation' values of each layer.
 x_propogated = []
+x_all_propogated = []
 
 #number of ouputs
 k = 1
@@ -30,51 +33,22 @@ l = 0
 #number of data sets
 m = 7
 
-learning_rate = 0.05
+learning_rate = 1
 lambada = 0.0005
-max_iterations = 5000 #5000
+max_iterations = 1000 #5000
+minimum_loss = 0.05
 
 
 def Initialise(only_param = False):
-    global x_propogated, m, l, k, total_delta, small_delta, y_list, x_list, parameters
+    global x_propogated, m, l, k, total_delta, small_delta, y_list, new_y_list, x_list, parameters
 
     #Initialises the parameters of each layer to random values.
-    parameters = [np.random.normal(0, 1, size= (200, 625)), np.random.normal(0, 1, size= (10, 201)), np.random.normal(0, 1, size =(1, 11))]
+    parameters = [np.random.normal(0, 1, size= (100, 625)), np.random.normal(0, 1, size= (10, 101)), np.random.normal(0, 1, size =(1, 11))]
     if only_param:
         return
 
     print("\nBeginning initialisation...")
 
-    '''
-    raw_x_list = os.listdir(os.path.join(sys.path[0], 'numbers'))
-    for x in raw_x_list:
-
-        im = np.array(Image.open(os.path.join(sys.path[0], 'numbers', x)))
-        im = im[:, :, 0]
-        i = 0
-        holder = im[0]
-        while i < im.shape[0]:
-            if i == 0:
-                i += 1
-                continue
-            holder = np.hstack((holder, im[i]))
-            i += 1
-
-        i = 0
-        while i < holder.shape[0]:
-            if holder[i] >= 200:
-                holder[i] = 0
-            else:
-                holder[i] = 1
-            i += 1
-
-        if x == '0_0.png':
-            x_list = np.array(holder)
-            y_list = np.array(0)
-            continue
-        x_list = np.vstack((x_list, holder))
-        y_list = np.hstack((y_list, float(x[0])))
-    '''
 
     x_list = np.load(os.path.join(sys.path[0], 'data', "x_list.npy"))
     y_list = np.load(os.path.join(sys.path[0], 'data', "y_list.npy"))
@@ -82,6 +56,7 @@ def Initialise(only_param = False):
     #Creates the shape of x_propogated from the list of parameters
     for param in parameters:
         x_propogated.append(np.array(param))
+        x_all_propogated.append(np.array(param))
     
     m = len(y_list)
     l = len(parameters)
@@ -90,10 +65,41 @@ def Initialise(only_param = False):
     for y in y_list:
         if y > k:
             k = y
+        
+    j = 0
+    while j <= k:
+        temp = []
+        i = 0
+        while i < m:
+            temp.append(y_list[i])
+            if not temp[i] == j:
+                temp[i] = 0
+            else:
+                temp[i] = 1
+            i += 1
+            
+        new_y_list.append(np.array(temp))
+        j += 1
   
 def Sigmoid(x, param):
-    z = (np.matmul(x,  np.transpose(param)))
-    return 1 / (1 + np.exp(np.negative(z)))
+    return 1 / (1 + np.exp(np.negative(np.matmul(x,  np.transpose(param)))))
+
+def Forward_Propogate_All(x_row):
+    global x_propogated, parameters
+
+    #Calculates the activation value of the first layer
+    x_all_propogated[0] = Sigmoid(x_row, parameters[0])
+    #Adds a bias unit to x_propogated (activation) so that it can be immediately used to calculate the activation of the next layer
+    x_all_propogated[0] = np.hstack((np.ones((x_all_propogated[0].shape[0], 1)), x_all_propogated[0]))
+
+    i = 1
+    while i < l:
+        #Calculates the activation value of every subsequent layer
+        x_all_propogated[i] = Sigmoid(x_all_propogated[i - 1], parameters[i])
+        if i != l - 1:
+            #If this is not the last activation value(neural network output) a bias unit is added to x_propgated
+            x_all_propogated[i] = np.hstack((np.ones((x_all_propogated[i].shape[0], 1)), x_all_propogated[i]))
+        i += 1
 
 def Forward_Propogate(x_row, special_parameters = None):
     global x_propogated, parameters
@@ -143,25 +149,15 @@ def Regularise_Parameters(parameters, sum_ = True):
         list_ = []
         for parameter in parameters:
             list_.append(parameter * lambada)
-
         return list_
 
     r = 0
     for parameter in parameters:
-        i = 0
-        while i < parameter.shape[0]:
-            if len(parameter.shape) > 1:
-                j = 0
-                while j < parameter.shape[1]:
-                    r += parameter[i,j] ** 2
-                    j += 1
-            else:
-                r += parameter[i] ** 2
-            i += 1
+        r += np.sum(parameter ** 2)
     return r * lambada
 
 def Loss(class_return = -1):
-    global x_list, y_list, x_propogated, m, l, parameters
+    global x_list, y_list, x_propogated, x_all_propogated, m, l, parameters
     #Calculates the total error of the neural network 
     '''
     i = 0
@@ -178,23 +174,9 @@ def Loss(class_return = -1):
     cost = []
     class_ = 0
     while class_ <= k:
-        new_y_list = []
-        i = 0
-        while i < m:
-            new_y_list.append(y_list[i])
-            if not y_list[i] == class_:
-                new_y_list[i] = 0
-            else:
-                new_y_list[i] = 1
-            i += 1
-        new_y_list = np.array(new_y_list)
-
-        h = []
-        for x in x_list:
-            Forward_Propogate(x)
-            h.append(x_propogated[l - 1])
-        h = np.transpose(np.array(h)[np.newaxis])
-        cost.append((np.matmul(np.negative(new_y_list).T, np.log(h)) - np.matmul((1 - new_y_list).T, np.log(1 - h)) + Regularise_Parameters(parameters)) / m)
+        Forward_Propogate_All(x_list)
+        h = x_all_propogated[l - 1]
+        cost.append((np.matmul(np.negative(new_y_list[class_]).T, np.log(h)) - np.matmul((1 - new_y_list[class_]).T, np.log(1 - h)) + Regularise_Parameters(parameters)) / m)
         class_ += 1  
 
     if class_return < 0:
@@ -216,21 +198,9 @@ def Load_Parameters():
         i += 1
 
 def Back_Propogation(class_):
-    global l, x_list, y_list, small_delta, total_delta, parameters, m
+    global l, x_list, y_list, new_y_list, small_delta, total_delta, parameters, m
     small_delta.clear()
     total_delta.clear()
-
-    #Generates new_y_list relative to given class
-    new_y_list = []
-    i = 0
-    while i < m:
-        new_y_list.append(y_list[i])
-        if not y_list[i] == class_:
-            new_y_list[i] = 0
-        else:
-            new_y_list[i] = 1
-        i += 1
-    new_y_list = np.array(new_y_list)
 
     #Creates the shape of the change varables
     for param in parameters:
@@ -241,46 +211,39 @@ def Back_Propogation(class_):
     while i < m:
         #Finds x_propogated for specified x value
         Forward_Propogate(x_list[i])
-
         l_ = l - 1
         while l_ > -1:
             #if at last layer in neural network the error of the layer is calculated for a specific x-value
             if l_ == l - 1:
-                small_delta[l_] =  x_propogated[l_] - new_y_list[i]
+                small_delta[l_] =  x_propogated[l_] - new_y_list[class_][i]
             
             #the error of each subsequent layer is calculated  for a specific x-value
             else:
-                sigmoid_derivative = x_propogated[l_] * (1 - x_propogated[l_])
-                sigmoid_derivative = np.delete(sigmoid_derivative, 0)
+                #sigmoid deriv
+                #sigmoid_derivative = x_propogated[l_] * (1 - x_propogated[l_])
+                #igmoid_derivative = np.delete(sigmoid_derivative, 0)
                 temp = np.transpose(parameters[l_ + 1]) * small_delta[l_ + 1]
                 #removing the bias unit from this array
                 temp = np.delete(np.transpose(temp), 0, 1)
                 if temp.shape[0] == 1:
                     temp = temp[0,...]
-                if len(temp.shape) > 1:
-                    if temp.shape[0] >= 1:
-                        temp = np.sum(temp,0)
-                small_delta[l_] = temp * sigmoid_derivative
+                if len(temp.shape) > 1 and temp.shape[0] >= 1:
+                    temp = np.sum(temp,0)
+                small_delta[l_] = temp * np.delete(x_propogated[l_] * (1 - x_propogated[l_]), 0)
             l_ -= 1
-
         l_ = l - 1
         while l_ > -1:
             #The error of the first layer is calculated using the input x values and added to the total error
             if l_ == 0:
-                total_delta[l_] += np.transpose(small_delta[l_][np.newaxis]) * x_list[i]
+                total_delta[l_] += (np.transpose(small_delta[l_][np.newaxis]) * x_list[i]) / m
             
             #the error of the each other layer is calculated using the propogated x values and added to the total error
             else:
-                total_delta[l_] += np.transpose(small_delta[l_][np.newaxis]) * x_propogated[l_ - 1]
+                total_delta[l_] += (np.transpose(small_delta[l_][np.newaxis]) * x_propogated[l_ - 1]) / m
             l_ -= 1
-            
 
         i += 1
     
-    #At the end of back-propogation the averaged total_delta of all layers are found
-    for change in total_delta:
-        change /= m
-
 def Gradient_Descent():
     global parameters, parameters_result, max_iterations, k, total_delta
     print("Beginning gradient descent...\n")
@@ -297,17 +260,30 @@ def Gradient_Descent():
     class_ = 0
     while class_ <= k:
         i = 0
+        lowest_loss = 10000000
         Initialise(True)
         print(f"Calculating parameters for class {class_}")
+        t = time.time()
 
         while i < max_iterations:
-            if i % 1000 == 0:
-                print(f"Loss: {Loss(class_)}\tIteration: {i}\n")
-                #print(f"Parameters: {parameters}")
-            elif i == max_iterations - 1:
-                print(f"Loss: {Loss(class_)}\tIteration: {i}\n")
-                #print(f"Parameters: {parameters}")
+            cost = Loss(class_)
+            if i % 20 == 0:
+                print(f"Loss: {cost}\tIteration: {i}")
+                if not i == 0:
+                    print(f"Time taken: {round(time.time() - t, 2)}")
+                    t = time.time()
+                print("\n")
 
+            elif i == max_iterations - 1:
+                print(f"Loss: {cost}\tIteration: {i}")
+                print(f"Time taken: {round(time.time() - t, 2)}\n")
+            
+            elif cost <= minimum_loss or cost > lowest_loss:
+                print(f"LowestLoss: {cost}\tIteration: {i}")
+                print(f"Time taken: {round(time.time() - t, 2)}\n")
+                i = max_iterations
+
+            lowest_loss = cost
             Back_Propogation(class_)
             
             #updates parameters
@@ -339,7 +315,7 @@ def Gradient_Descent():
     '''
 
 def TestNetwork():
-    global k, parameters_result, x_propogated
+    global k, l, parameters_result, x_propogated
     print("\nEnter anything besides a number into the command line to quit or press the x on the gui to quit.\n")
     while True:
         print("Draw number")
@@ -351,12 +327,22 @@ def TestNetwork():
 
         Forward_Propogate(x_list_input)
 
+        probabilities = np.zeros((3,2))
         class_ = 0
         while class_ < k + 1:
             Forward_Propogate(x_list_input, parameters_result[class_])
-            print(f"Probablility of entered values equaling {class_} is: {(x_propogated[l - 1])}")
+            if x_propogated[l - 1] > probabilities[0,1]:
+                probabilities[0] = np.array([class_, x_propogated[l-1]])
+            elif x_propogated[l - 1] > probabilities[1,1]:
+                probabilities[1] = np.array([class_, x_propogated[l-1]])
+            elif x_propogated[l - 1] > probabilities[2,1]:
+                probabilities[2] = np.array([class_, x_propogated[l-1]])
             class_ += 1
+        print(f"Probability of input equaling {probabilities[0,0]} is {probabilities[0,1]}")
+        print(f"Probability of input equaling {probabilities[1,0]} is {probabilities[1,1]}")
+        print(f"Probability of input equaling {probabilities[2,0]} is {probabilities[2,1]}")
         print()
+
 
 if train:
     Initialise()
@@ -367,3 +353,18 @@ else:
     Initialise()
     Load_Parameters()
     TestNetwork()
+
+'''
+Initialise()
+
+t = time.time()
+Loss()
+print(f"lo {time.time()-t}")
+t = time.time()
+Back_Propogation(1)
+print(f"bp {time.time()-t}")
+t = time.time()
+Forward_Propogate(x_list[0])
+print(f"fp {time.time()-t}")
+print(new_y_list[0])
+'''
